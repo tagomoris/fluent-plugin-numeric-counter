69ef6efd9d4278f2165ef06638b04ad2ef604541
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-07-04 17:33:46 +0900

    first implementation

--- lib/fluent/plugin/out_numeric_counter.rb
+++ lib/fluent/plugin/out_numeric_counter.rb
@@ -12,12 +12,29 @@ class Fluent::NumericCounterOutput < Fluent::Output
   config_param :outcast_unmatched, :bool, :default => false
 
   # pattern0 reserved as unmatched counts
-  config_param :pattern1, :string # string: NAME LOW-HIGH
+  config_param :pattern1, :string # string: NAME LOW HIGH
+                                  # LOW/HIGH allows size prefix (ex: 10k, 5M, 3500G)
   (2..PATTERN_MAX_NUM).each do |i|
     config_param ('pattern' + i.to_s).to_sym, :string, :default => nil
   end
   
-  attr_accessor :tick, :counts, :last_checked
+  attr_accessor :counts, :last_checked
+
+  # for test
+  # attr_accessor :count_interval, :unit, :aggregate, :tag, :input_tag_remove_prefix, :count_key, :outcast_unmatched
+  attr_accessor :patterns
+
+  def parse_num(str)
+    if str.nil?
+      nil
+    elsif str =~ /^[-0-9]+$/
+      str.to_i
+    elsif str =~ /^[-.0-9]+$/
+      str.to_f
+    else
+      Fluent::Config.size_value(str)
+    end
+  end
 
   def configure(conf)
     super
@@ -35,9 +52,31 @@ class Fluent::NumericCounterOutput < Fluent::Output
     @aggregate = @aggregate.to_sym
     raise Fluent::ConfigError, "numeric_counter allows tag/all to aggregate unit" unless [:tag, :all].include?(@aggregate)
 
-    @patterns = []
-    #TODO ato de kaku
+    @patterns = [[0, 'unmatched', nil, nil]] # counts-index, name, low, high
+    pattern_names = ['unmatched']
 
+    invalids = conf.keys.select{|k| k =~ /^pattern(\d+)$/ and not (1..PATTERN_MAX_NUM).include?($1.to_i)}
+    if invalids.size > 0
+      $log.warn "invalid number patterns (valid pattern number:1-#{PATTERN_MAX_NUM}):" + invalids.join(",")
+    end
+    (1..PATTERN_MAX_NUM).each do |i|
+      next unless conf["pattern#{i}"]
+      name,low,high = conf["pattern#{i}"].split(/ +/, 3)
+      @patterns.push([i, name, parse_num(low), parse_num(high)])
+      pattern_names.push(name)
+    end
+    pattern_index_list = conf.keys.select{|s| s =~ /^pattern\d$/}.map{|v| (/^pattern(\d)$/.match(v))[1].to_i}
+    unless pattern_index_list.reduce(true){|v,i| v and @patterns[i]}
+      raise Fluent::ConfigError, "jump of pattern index found"
+    end
+    unless @patterns.length == pattern_names.uniq.length
+      raise Fluent::ConfigError, "duplicated pattern names found"
+    end
+    @patterns[1..-1].each do |index, name, low, high|
+      raise Fluent::ConfigError, "numbers of low/high missing" if low.nil?
+      raise Fluent::ConfigError, "unspecified high threshold allowed only in last pattern" if high.nil? and index != @patterns.length - 1
+    end
+    
     if @input_tag_remove_prefix
       @removed_prefix_string = @input_tag_remove_prefix + '.'
       @removed_length = @removed_prefix_string.length
@@ -59,38 +98,123 @@ class Fluent::NumericCounterOutput < Fluent::Output
   end
 
   def count_initialized(keys=nil)
-    #TODO ato de kaku
+    # counts['tag'][pattern_index_num] = count
+    if @aggregate == :all
+      {'all' => Array.new(@patterns.length){|i| 0}}
+    elsif keys
+      values = Array.new(keys.length){|i|
+        Array.new(@patterns.length){|j| 0 }
+      }
+      Hash[[keys, values].transpose]
+    else
+      {}
+    end
   end
 
   def countups(tag, counts)
-    #TODO ato de kaku
+    if @aggregate == :all
+      tag = 'all'
+    end
+
+    @mutex.synchronize {
+      @counts[tag] ||= [0] * @patterns.length
+      counts.each_with_index do |count, i|
+        @counts[tag][i] += count
+      end
+    }
   end
 
   def stripped_tag(tag)
-    #TODO ato de kaku
+    return tag unless @input_tag_remove_prefix
+    return tag[@removed_length..-1] if tag.start_with?(@removed_prefix_string) and tag.length > @removed_length
+    return tag[@removed_length..-1] if tag == @input_tag_remove_prefix
+    tag
   end
 
   def generate_output(counts, step)
-    #TODO ato de kaku
+    output = {}
+
+    if @aggregate == :all
+      # index 0 is unmatched
+      sum = if @outcast_unmatched
+              counts['all'][1..-1].inject(:+)
+            else
+              counts['all'].inject(:+)
+            end
+      counts['all'].each_with_index do |count,i|
+        name = @patterns[i][1]
+        output[name + '_count'] = count
+        output[name + '_rate'] = ((count * 100.0) / (1.00 * step)).floor / 100.0
+        unless i == 0 and @outcast_unmatched
+          output[name + '_percentage'] = count * 100.0 / (1.00 * sum) if sum > 0
+        end
+      end
+      return output
+    end
+
+    counts.keys.each do |tag|
+      t = stripped_tag(tag)
+      sum = if @outcast_unmatched
+              counts[tag][1..-1].inject(:+)
+            else
+              counts[tag].inject(:+)
+            end
+      counts[tag].each_with_index do |count,i|
+        name = @patterns[i][1]
+        output[t + '_' + name + '_count'] = count
+        output[t + '_' + name + '_rate'] = ((count * 100.0) / (1.00 * step)).floor / 100.0
+        unless i == 0 and @outcast_unmatched
+          output[t + '_' + name + '_percentage'] = count * 100.0 / (1.00 * sum) if sum > 0
+        end
+      end
+    end
+    output
   end
 
   def flush(step)
-    #TODO
+    flushed,@counts = @counts,count_initialized(@counts.keys.dup)
+    generate_output(flushed, step)
   end
 
   def flush_emit(step)
-    #TODO
+    Fluent::Engine.emit(@tag, Fluent::Engine.now, flush(step))
   end
 
   def start_watch
-    #TODO
+    @watcher = Thread.new(&method(:watch))
   end
 
   def watch
-    #TODO
+    @last_checked = Fluent::Engine.now
+    while true
+      sleep 0.5
+      if Fluent::Engine.now - @last_checked >= @count_interval
+        now = Fluent::Engine.now
+        flush_emit(now - @last_checked)
+        @last_checked = now
+      end
+    end
   end
 
   def emit(tag, es, chain)
-    #TODO
+    c = [0] * @patterns.length
+
+    es.each do |time,record|
+      value = record[@count_key]
+      next if value.nil?
+
+      value = value.to_f
+      matched = false
+      @patterns.each do |index, name, low, high|
+        next if low.nil? or value < low or value >= high
+        c[index] += 1
+        matched = true
+        break
+      end
+      c[0] += 1 unless matched
+    end
+    countups(tag, c)
+
+    chain.next
   end
 end
--- test/plugin/test_out_numeric_counter.rb
+++ test/plugin/test_out_numeric_counter.rb
@@ -6,16 +6,185 @@ class NumericCounterOutputTest < Test::Unit::TestCase
   end
 
   CONFIG = %[
+    count_interval 60
+    aggregate tag
+    input_tag_remove_prefix test
+    count_key target
+    pattern1 u100ms 0 100000
+    pattern2 u1s 100000 1000000
+    pattern3 u3s 1000000 3000000
   ]
 
   def create_driver(conf=CONFIG, tag='test')
     Fluent::Test::OutputTestDriver.new(Fluent::NumericCounterOutput, tag).configure(conf)
   end
   
+  def test_parse_num
+    p = create_driver.instance
+
+    assert_equal 1, p.parse_num('1')
+    assert_equal -1, p.parse_num('-1')
+    assert_equal 1.0, p.parse_num('1.0')
+    assert_equal -2.0, p.parse_num('-2.0000')
+    assert_equal 1024, p.parse_num('1k')
+  end
+
   def test_configure
-    
+    d = create_driver %[
+      count_key field1
+      pattern1 smallnum 0.1 200
+      pattern2 subnum   -500 -1
+    ]
+
+    assert_equal 60, d.instance.count_interval
+    assert_equal :tag, d.instance.aggregate
+    assert_equal 'numcount', d.instance.tag
+    assert_nil d.instance.input_tag_remove_prefix
+    assert_equal false, d.instance.outcast_unmatched
+    assert_equal [[0, 'unmatched', nil, nil], [1, 'smallnum', 0.1, 200], [2, 'subnum', -500, -1]], d.instance.patterns
+
+    d = create_driver %[
+      count_key key1
+      pattern1 x 0.1 10
+      pattern2 y 10 11
+      pattern3 z 11
+    ]
+    assert_equal [[0, 'unmatched', nil, nil], [1, 'x', 0.1, 10], [2, 'y', 10, 11], [3, 'z', 11, nil]], d.instance.patterns
+  end
+
+  def test_countups
+    d = create_driver
+    assert_nil d.instance.counts['test.input']
+
+    d.instance.countups('test.input', [0, 0, 0, 0])
+    assert_equal [0,0,0,0], d.instance.counts['test.input']
+    d.instance.countups('test.input', [1, 1, 1, 0])
+    assert_equal [1,1,1,0], d.instance.counts['test.input']
+    d.instance.countups('test.input', [0, 5, 1, 0])
+    assert_equal [1,6,2,0], d.instance.counts['test.input']
+  end
+
+  def test_generate_output
+    d = create_driver
+    # pattern1 u100ms 0 100000
+    # pattern2 u1s 100000 1000000
+    # pattern3 u3s 1000000 3000000
+
+    r1 = d.instance.generate_output({'test.input' => [60,240,180,120], 'test.input2' => [0,600,0,0]}, 60)
+    assert_equal   60, r1['input_unmatched_count']
+    assert_equal  1.0, r1['input_unmatched_rate']
+    assert_equal 10.0, r1['input_unmatched_percentage']
+    assert_equal  240, r1['input_u100ms_count']
+    assert_equal  4.0, r1['input_u100ms_rate']
+    assert_equal 40.0, r1['input_u100ms_percentage']
+    assert_equal  180, r1['input_u1s_count']
+    assert_equal  3.0, r1['input_u1s_rate']
+    assert_equal 30.0, r1['input_u1s_percentage']
+    assert_equal  120, r1['input_u3s_count']
+    assert_equal  2.0, r1['input_u3s_rate']
+    assert_equal 20.0, r1['input_u3s_percentage']
+
+    assert_equal    0, r1['input2_unmatched_count']
+    assert_equal  0.0, r1['input2_unmatched_rate']
+    assert_equal  0.0, r1['input2_unmatched_percentage']
+    assert_equal  600, r1['input2_u100ms_count']
+    assert_equal 10.0, r1['input2_u100ms_rate']
+    assert_equal 100.0, r1['input2_u100ms_percentage']
+    assert_equal    0, r1['input2_u1s_count']
+    assert_equal  0.0, r1['input2_u1s_rate']
+    assert_equal  0.0, r1['input2_u1s_percentage']
+    assert_equal    0, r1['input2_u3s_count']
+    assert_equal  0.0, r1['input2_u3s_rate']
+    assert_equal  0.0, r1['input2_u3s_percentage']
+
+    d = create_driver %[
+      aggregate all
+      count_key f1
+      pattern1 good 1 2
+      outcast_unmatched yes
+    ]
+    r2 = d.instance.generate_output({'all' => [60,240]}, 60)
+    assert_equal  60, r2['unmatched_count']
+    assert_equal 1.0, r2['unmatched_rate']
+    assert_nil r2['unmatched_percentage']
+    assert_equal 240, r2['good_count']
+    assert_equal 4.0, r2['good_rate']
+    assert_equal 100.0, r2['good_percentage']
+  end
+
+  def test_pattern_num
+    assert_equal 20, Fluent::NumericCounterOutput::PATTERN_MAX_NUM
+
+    conf = %[
+      aggregate all
+      count_key field
+    ]
+    (1..20).each do |i|
+      conf += "pattern#{i} name#{i} #{i} #{i+1}\n"
+    end
+    d = create_driver(conf, 'test.max')
+    d.run do
+      (0..21).each do |i|
+        d.emit({'field' => i})
+      end
+    end
+    r = d.instance.flush(60)
+    assert_equal 2, r['unmatched_count'] # 0 and 21
+    assert_equal 1, r['name1_count']
+    assert_equal 1, r['name2_count']
+    assert_equal 1, r['name3_count']
+    assert_equal 1, r['name4_count']
+    assert_equal 1, r['name5_count']
+    assert_equal 1, r['name6_count']
+    assert_equal 1, r['name7_count']
+    assert_equal 1, r['name8_count']
+    assert_equal 1, r['name9_count']
+    assert_equal 1, r['name10_count']
+    assert_equal 1, r['name11_count']
+    assert_equal 1, r['name12_count']
+    assert_equal 1, r['name13_count']
+    assert_equal 1, r['name14_count']
+    assert_equal 1, r['name15_count']
+    assert_equal 1, r['name16_count']
+    assert_equal 1, r['name17_count']
+    assert_equal 1, r['name18_count']
+    assert_equal 1, r['name19_count']
+    assert_equal 1, r['name20_count']
   end
 
   def test_emit
+  # CONFIG = %[
+  #   count_interval 60
+  #   aggregate tag
+  #   input_tag_remove_prefix test
+  #   count_key target
+  #   pattern1 u100ms 0 100000
+  #   pattern2 u1s 100000 1000000
+  #   pattern3 u3s 1000000 3000000
+  # ]
+    d = create_driver(CONFIG, 'test.tag1')
+    d.run do
+      60.times do
+        d.emit({'target' =>  '50000'})
+        d.emit({'target' => '100000'})
+        d.emit({'target' => '100001'})
+        d.emit({'target' => '0.0'})
+        d.emit({'target' => '-1'})
+      end
+    end
+    r = d.instance.flush(60)
+
+    assert_equal 120, r['tag1_u100ms_count']
+    assert_equal 2.0, r['tag1_u100ms_rate']
+    assert_equal 40.0, r['tag1_u100ms_percentage']
+    assert_equal 120, r['tag1_u1s_count']
+    assert_equal 2.0, r['tag1_u1s_rate']
+    assert_equal 40, r['tag1_u1s_percentage']
+    assert_equal 0, r['tag1_u3s_count']
+    assert_equal 0, r['tag1_u3s_rate']
+    assert_equal 0, r['tag1_u3s_percentage']
+    assert_equal 60, r['tag1_unmatched_count']
+    assert_equal 1.0, r['tag1_unmatched_rate']
+    assert_equal 20, r['tag1_unmatched_percentage']
   end
 end
